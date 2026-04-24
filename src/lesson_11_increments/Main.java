b190ded62c8bc58548ef25d3b36567bda2fa6891
fedotiuk-dm <fedotiukdm@gmail.com>
2026-04-24 17:28:55 +0200

    lesson 12: інкременти

---
+++ src/lesson_11_increments/Main.java
@@ -0,0 +1,12 @@
+package lesson_11_increments;
+
+public class Main {
+    void main() {
+
+        int i = 1;
+        int a = ++i; // колим міняєш місцями ++ між і, то рахує по різному зліва на право, і з права на ліво.
+
+        System.out.println(i);
+        System.out.println(a);
+    }
+}
