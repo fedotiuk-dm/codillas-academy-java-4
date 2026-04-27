b486e5e3dc9e30af72b1279a0c1473250bd92d6b
fedotiuk-dm <fedotiukdm@gmail.com>
2026-04-27 21:41:42 +0200

    HW lesson 13: scanner console input 2d version

--- src/lesson_13_scanner/Main.java
+++ src/lesson_13_scanner/Main.java
@@ -6,14 +6,18 @@ public class Main {
     void main() {
         Scanner console = new Scanner(System.in);
 
-        System.out.print("Введіть розмір масиву: ");
-        int size = console.nextInt();
+        System.out.print("Введіть розмір масиву N: ");
+        int sizeN = console.nextInt();
+        System.out.print("Введіть розмір масиву M: ");
+        int sizeM = console.nextInt();
 
-        int[] numbers = new int[size];
+        int[][] numbers = new int[sizeN][sizeM];
 
         for (int i = 0; i < numbers.length; i++) {
-            System.out.print("Введіть елемент масиву " + i + ": ");
-            numbers[i] = console.nextInt();
+            for (int j = 0; j < numbers[i].length; j++) {
+                System.out.print("Введіть елемент масиву [N" + i + "][M" + j + "]: ");
+                numbers[i][j] = console.nextInt();
+            }
         }
 
         int choice;
@@ -28,18 +32,23 @@ public class Main {
             choice = console.nextInt();
 
             int sum = 0;
-            for (int number : numbers)
-                sum += number;
+            for (int[] row : numbers) {
+                for (int number : row) {
+                    sum += number;
+                }
+            }
 
             switch (choice) {
                 case 1 -> System.out.println("Сума всіх елементів = " + sum);
                 case 2 ->
-                        System.out.println("Середнє арифметичне значення елементу = " + (double) sum / numbers.length);
+                        System.out.println("Середнє арифметичне значення елементу = " + (double) sum / (sizeN * sizeM));
                 case 3 -> {
-                    int max = numbers[0];
+                    int max = numbers[0][0];
                     for (int i = 1; i < numbers.length; i++) {
-                        if (numbers[i] > max) {
-                            max = numbers[i];
+                        for (int j = 0; j < numbers.length; j++) {
+                            if (numbers[i][j] > max) {
+                                max = numbers[i][j];
+                            }
                         }
                     }
                     System.out.println("Максимальне значення елементу = " + max);
