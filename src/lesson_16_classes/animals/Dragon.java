94909931b302bca10481f2fbde3a72487f0065c0
fedotiuk-dm <fedotiukdm@gmail.com>
2026-05-06 13:10:47 +0200

    HW lesson 18: ENUM breeTypes dragon choice

--- src/lesson_16_classes/animals/Dragon.java
+++ src/lesson_16_classes/animals/Dragon.java
@@ -81,7 +81,7 @@ public class Dragon {
                 ", weight=" + weight +
                 ", voice='" + voice + '\'' +
                 ", fire='" + fire + '\'' +
-                ", breed=" + breedType +
+                ", breedType=" + breedType +
                 '}';
     }
 }
